55a1a4974abac1ab3dee608bbd6235864459759d
Cam <cam@expx.dev>
2025-05-01 10:49:12 -0600

    fix: include gradle for test-shard

---
+++ test-shard/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("jvm")
+    id("com.google.devtools.ksp") version "2.1.20-1.0.32"
+}
+
+group = "host.minestudio"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+
+    compileOnly(project(":api"))
+    compileOnly("net.minestom:minestom-snapshots:87f6524aeb")
+    compileOnly("com.google.auto.service:auto-service:1.1.1")
+    ksp("dev.zacsweers.autoservice:auto-service-ksp:1.2.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
